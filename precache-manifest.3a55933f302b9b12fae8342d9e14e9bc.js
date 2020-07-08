42e9ebef53627ba3c4473c76c4712cafed950d0e
Farkhod Isoev <nanotelegram@gmail.com>
2020-07-08 13:27:31 +0700

    Updates

---
+++ precache-manifest.3a55933f302b9b12fae8342d9e14e9bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539bc2e6461f449d659355f0391e9811",
+    "url": "/nanotelegram/git-test-react.git/index.html"
+  },
+  {
+    "revision": "da65b63121fefbab527f",
+    "url": "/nanotelegram/git-test-react.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e10131e1587f687f514b",
+    "url": "/nanotelegram/git-test-react.git/static/js/2.b3dc571a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nanotelegram/git-test-react.git/static/js/2.b3dc571a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da65b63121fefbab527f",
+    "url": "/nanotelegram/git-test-react.git/static/js/main.de204abc.chunk.js"
+  },
+  {
+    "revision": "752bd6462b2df052e15e",
+    "url": "/nanotelegram/git-test-react.git/static/js/runtime-main.2e4b0e72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nanotelegram/git-test-react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
